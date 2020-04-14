caa75e327c8307e02d9dffb45f552b1aa39e5169
Nikolay Norkin <nikolay.norkin@forward3d.com>
2020-04-14 16:26:07 +0100

    separate eye movement

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+function updateEye(eye, pointerX, pointerY) {
+  var x = (eye.offset().left) + (eye.width() / 2);
+  var y = (eye.offset().top) + (eye.height() / 2);
+  var rad = Math.atan2(pointerX - x, pointerY - y);
+  var rot = (rad * (180 / Math.PI) * -1) + 180;
+  eye.css({
+    '-webkit-transform': 'rotate(' + rot + 'deg)',
+    '-moz-transform': 'rotate(' + rot + 'deg)',
+    '-ms-transform': 'rotate(' + rot + 'deg)',
+    'transform': 'rotate(' + rot + 'deg)'
+  });
+}
 $(function () {
   $("body").mousemove(function(event) {
-    var eye = $(".eye");
-    var x = (eye.offset().left) + (eye.width() / 2);
-    var y = (eye.offset().top) + (eye.height() / 2);
-    var rad = Math.atan2(event.pageX - x, event.pageY - y);
-    var rot = (rad * (180 / Math.PI) * -1) + 180;
-    eye.css({
-      '-webkit-transform': 'rotate(' + rot + 'deg)',
-      '-moz-transform': 'rotate(' + rot + 'deg)',
-      '-ms-transform': 'rotate(' + rot + 'deg)',
-      'transform': 'rotate(' + rot + 'deg)'
-    });
+    x = event.pageX
+    y = event.pageY
+    updateEye($('#left_eye'), x, y)
+    updateEye($('#right_eye'), x, y)
   });
 });
