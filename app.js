9f966164fae23766f9fcaaf7b8315a3d43aa597a
Nikolay Norkin <nikolay.norkin@forward3d.com>
2020-04-15 00:16:41 +0100

    ngingx dc

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 function updateEye(eye, pointerX, pointerY) {
-  var x = (eye.offset().left) + (eye.width() / 2);
-  var y = (eye.offset().top) + (eye.height() / 2);
-  var rad = Math.atan2(pointerX - x, pointerY - y);
-  var rot = (rad * (180 / Math.PI) * -1) + 180;
-  eye.css({
-    '-webkit-transform': 'rotate(' + rot + 'deg)',
-    '-moz-transform': 'rotate(' + rot + 'deg)',
-    '-ms-transform': 'rotate(' + rot + 'deg)',
-    'transform': 'rotate(' + rot + 'deg)'
-  });
+    var x = (eye.offset().left) + (eye.width() / 2);
+    var y = (eye.offset().top) + (eye.height() / 2);
+    var rad = Math.atan2(pointerX - x, pointerY - y);
+    var rot = (rad * (180 / Math.PI) * -1) + 180;
+    eye.css({
+        '-webkit-transform': 'rotate(' + rot + 'deg)',
+        '-moz-transform': 'rotate(' + rot + 'deg)',
+        '-ms-transform': 'rotate(' + rot + 'deg)',
+        'transform': 'rotate(' + rot + 'deg)'
+    });
 }
+
+function handleOrientation(event) {
+    var absolute = event.absolute;
+    var alpha = event.alpha;
+    var beta = event.beta;
+    var gamma = event.gamma;
+
+    rot = -1 * gamma
+
+    var eye = $('.eye')
+    eye.css({
+        '-webkit-transform': 'rotate(' + rot + 'deg)',
+        '-moz-transform': 'rotate(' + rot + 'deg)',
+        '-ms-transform': 'rotate(' + rot + 'deg)',
+        'transform': 'rotate(' + rot + 'deg)'
+    });
+}
+
 $(function () {
-  $("body").mousemove(function(event) {
-    x = event.pageX
-    y = event.pageY
-    updateEye($('#left_eye'), x, y)
-    updateEye($('#right_eye'), x, y)
-  });
+    $("body").mousemove(function (event) {
+        x = event.pageX
+        y = event.pageY
+        updateEye($('#left_eye'), x, y)
+        updateEye($('#right_eye'), x, y)
+    });
+
+    window.addEventListener("deviceorientation", handleOrientation, true);
 });
